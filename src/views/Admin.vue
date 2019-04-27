9c0aa06c7c5e3bde406cbc29b77c9b41a14ba536
mlau95 <mattlau1218@gmail.com>
2019-04-27 16:57:10 -0700

    added drop-down list of talents created display of performers

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,23 +1,23 @@
 <template>
 	<div class="admin">
 		<h1 class="subheading grey--text">User List</h1>
-		<v-container class="my-5">
-			<v-card flat class="pa-3" v-for="user in users" :key="user.username">
-				<v-layout row wrap>
-					<v-flex xs6 sm4 md2>
-						<div class="caption grey--text">Username</div>
-						<div>{{ user.username }}</div>
-					</v-flex>
-					<v-flex xs6 sm4 md2>
-						<div class="caption grey--text">Full Name</div>
-						<div>{{ user.fullname }}</div>
-					</v-flex>
-					<v-flex xs6 sm4 md2>
-						<div class="caption grey--text">Admin</div>
-						<div>{{ user.admin }}</div>
-					</v-flex>
-				</v-layout>
-			</v-card>
+			<v-container class="my-5">
+				<v-card flat class="pa-3" v-for="user in users" :key="user.username">
+					<v-layout row wrap>
+						<v-flex xs6 sm4 md2>
+							<div class="caption grey--text">Username</div>
+							<div>{{ user.username }}</div>
+						</v-flex>
+						<v-flex xs6 sm4 md2>
+							<div class="caption grey--text">Full Name</div>
+							<div>{{ user.fullname }}</div>
+						</v-flex>
+						<v-flex xs6 sm4 md2>
+							<div class="caption grey--text">Admin</div>
+							<div>{{ user.admin }}</div>
+						</v-flex>
+					</v-layout>
+				</v-card>
 		</v-container>
 	</div>
 </template>
--- src/views/Performers.vue
+++ src/views/Performers.vue
@@ -1,12 +1,83 @@
 <template>
   <div class="performer">
-    <h1>Performers</h1>
+    <h1 class="subheading grey--text">Performers List</h1>
+      <v-container class="my-5">
+        <v-flex xs12 sm6 d-flex>
+          <v-select
+            :items="talents"
+            label="Select talent"
+            v-model="selected_talent"
+            solo
+          ></v-select>
+          <v-btn class="sucess" @click="display">Search</v-btn>
+        </v-flex>
+        <v-card flat class="pa-3" v-for="performer in performers" :key="performer.fullname">
+          <v-layout row wrap>
+            <v-flex xs6 sm4 md2>
+              <div class="caption grey--text">Full Name</div>
+              <div>{{ performer.fullname }}</div>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-container>
   </div>
 </template>
 
 <script>
+  import { db } from '../firebase';
+
   export default {
-    name: "Performers"
+    data() {
+      return {
+        talents: [
+          { text: 'Dancer', value: "dancer" },
+          { text: 'Singer', value: "singer" },
+          { text: 'Musician', value: "musician" },
+        ],
+        selected_talent: '',
+        performers: [],
+      }
+    },
+    methods: {
+      display() {
+        this.performers = [];
+        db.collection('performers')
+        .where('talent', '==', this.selected_talent)
+        .get()
+        .then(doc => {
+          const changes = doc.docChanges();
+          changes.forEach(change => {
+            if(change.type == 'added') {
+              this.performers.push({
+                ...change.doc.data(),
+                id: change.doc.id
+              })
+            }
+          })
+        })
+
+            // .onSnapshot(res => {
+            //   const changes = res.docChanges();
+            //   changes.forEach(change => {
+            //     if(change.type == 'added'){
+            //       if(this.performers.length === 0){
+            //         this.performers.push({
+            //           ...change.doc.data(),
+            //           id: change.doc.id
+            //         })
+            //       }
+            //       else{
+            //         this.performers = [];
+            //         this.performers.push({
+            //           ...change.doc.data(),
+            //           id: change.doc.id
+            //         })
+            //       }
+            //     }
+            //   })
+            // })
+      }
+    }
   }
 </script>
 
