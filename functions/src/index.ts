33616c232f0c40d50270cb4c60ff015f8e60c218
Justin Pancho <jpancho@my.bcit.ca>
2019-05-03 13:49:08 -0700

    Fix typo breaking performer list

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -21,6 +21,22 @@ export const addUsers = functions.auth
     });
   });
 
+export const bookPerformer = functions.database
+  .ref('/performers/BSKOC1P0z0bwOIE6WI9uwmWKw2J3')
+  .onUpdate(((change) => {
+    //const userRef = '/users/xrZ4uAgdZAe1ol3Skg2f2EuMAmh2/performersBooked';
+    const before = change.before;
+    const after = change.after;
+    if (before === after) {
+      return null;
+    }
+
+    return admin.database()
+      .ref('users/xrZ4uAgdZAe1ol3Skg2f2EuMAmh2')
+      .set({after});
+    // return admin.database().ref(userRef).push({after});
+  }));
+
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
