7cced2bdadb9b4487cb440066d01fb349be69965
TacklessNebula3 <veerdhaliwal22@gmail.com>
2019-05-12 13:21:00 -0700

    - Added v-slot for PopupLogin. - Changed flat button to round for becoming a performer. - Changed display for profile page. - Added v-slot for Signout.

--- src/views/PopupLogin.vue
+++ src/views/PopupLogin.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
   <v-layout row justify-center>
     <v-dialog v-model="dialog" max-width="400">
       <template v-slot:activator="{ on }">
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="profile">
-    <h1 class="subheading grey--text">Profile Page</h1>
+    <v-toolbar-title class="text-uppercase grey--text">
+      <h1>
+        <span class="font-weight-light blue--text display-1" >Profile</span>
+      </h1>
+    </v-toolbar-title>
     <v-container class="my-5">
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg12 v-for="info in profile" :key="info.uid">
+          <v-card flat class="text-xs-center ma-3 white--text" color="black lighten-2">
+            <v-responsive class="pt-4">
+              <v-avatar size="100" class="green lighten-2">
+              </v-avatar>
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading font-weight-regular">Name</div>
+              <div class="grey--text">{{ info.email }}</div>
+              <p></p>
+              <div>ID</div>
+              <div class="grey--text">{{ info.uid }}</div>
+            </v-card-text>
+            <v-divider color="grey"></v-divider>
+            <v-divider color="grey"></v-divider>
+            <p></p>
+            <p class="font-italic font-weight-light">DESCRIPTION</p>
+            <p>...</p>
+          </v-card>
+        </v-flex>
+      </v-layout>
       <v-card flat class="pa-3" v-for="info in profile" :key="info.uid">
         <v-layout row wrap>
           <v-flex xs6 sm4 md2>
