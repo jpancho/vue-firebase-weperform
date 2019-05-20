431165a397e95f6378b3b21130fbff997e926ed9
jpancho <jpancho@sfu.ca>
2019-05-20 14:15:49 -0700

    Fix register form

--- src/views/PopupRegister.vue
+++ src/views/PopupRegister.vue
@@ -1,28 +1,39 @@
 <template>
   <v-layout row justify-center>
-    <v-dialog max-width="400">
+    <v-dialog v-model="dialog" max-width="400">
       <v-btn flat color="grey" slot="activator">Register</v-btn>
-      <v-card>
-        <v-text-field
-          prepend-icon="person"
-          label="Full name"
-          v-model="fullname"
-        ></v-text-field>
-        <v-text-field
-          prepend-icon="email"
-          label="Email"
-          v-model="email"
-        ></v-text-field>
-        <v-text-field
-          prepend-icon="lock"
-          v-model="password"
-          :type="'password'"
-          name="input-10-1"
-          label="Password"
-        ></v-text-field>
+      <v-card class="elevation-12">
+        <v-toolbar dark color="primary">
+          <v-toolbar-title>Register</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              prepend-icon="person"
+              label="Full name"
+              v-model="fullname"
+            ></v-text-field>
+            <v-text-field
+              prepend-icon="email"
+              label="Email"
+              v-model="email"
+            ></v-text-field>
+            <v-text-field
+              prepend-icon="lock"
+              v-model="password"
+              :type="'password'"
+              name="input-10-1"
+              label="Password"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn class="success" @click="register">Sign-up</v-btn>
+          <v-btn
+            color="primary"
+            @click="register">Register
+          </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
