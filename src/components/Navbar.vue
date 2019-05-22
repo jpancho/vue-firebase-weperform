863f1c3e14a407a15322a2c1eba6f081fd536153
Justin Pancho <jpancho@my.bcit.ca>
2019-05-22 10:31:10 -0700

    Space out navbar and hide based on auth

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,38 +2,28 @@
 	<nav>
 		<v-toolbar flat app>
 <!--			<v-toolbar-side-icon class="grey&#45;&#45;text" @click="drawer = !drawer"></v-toolbar-side-icon>-->
-			<v-toolbar-title class="text-uppercase grey--text">
+			<v-toolbar-title class="text-uppercase grey--text pr-3">
 				<span class="font-weight-light"><router-link to="/">We</router-link></span>
 				<span><router-link to="/">Perform</router-link></span>
 			</v-toolbar-title>
-      <v-btn flat color="grey" to="/performers">Search
-        <v-icon right small>search</v-icon>
-			</v-btn>
-			<v-btn flat color="grey" to="/bookings">Bookings
-        <v-icon right small>shopping_cart</v-icon>
-			</v-btn>
-<!--			<v-btn flat color="grey" to="/about">-->
-<!--				<span>About</span>-->
-<!--			</v-btn>-->
-			<!--<v-btn flat color="grey" to="/admin">-->
-				<!--<span>Admin</span>-->
-			<!--</v-btn>-->
-			<v-spacer></v-spacer>
-			<v-spacer></v-spacer>
-			<v-spacer></v-spacer>
-			<v-spacer></v-spacer>
-			<v-spacer></v-spacer>
-			<v-spacer></v-spacer>
-			<v-btn flat color="grey" to="/profile">Profile
-        <v-icon right small>face</v-icon>
-			</v-btn>
-      <template v-if="user">
-        <Signout/>
-      </template>
-      <template v-else>
-        <PopupRegister/>
-        <PopupLogin/>
-      </template>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn v-if="user" flat color="grey" to="/performers">Search
+          <v-icon right small>search</v-icon>
+        </v-btn>
+        <v-btn v-if="user" flat color="grey" to="/bookings">Bookings
+          <v-icon right small>shopping_cart</v-icon>
+        </v-btn>
+      </v-toolbar-items>
+
+        <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn v-if="user" flat color="grey" to="/profile">Profile
+          <v-icon right small>face</v-icon>
+        </v-btn>
+        <Signout v-if="user"/>
+        <PopupRegister v-if="user === null"/>
+        <PopupLogin v-if="user === null"/>
+      </v-toolbar-items>
 		</v-toolbar>
 
 <!--		<v-navigation-drawer app v-model="drawer" class="black">-->
