aa0be51e671fec94564edb4c0f43118c443a9960
jpancho <jpancho@sfu.ca>
2019-06-21 15:16:47 -0700

    Remove config

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,8 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/functions';
 
+// Removed configuration keys and put in .config file
 const config = {
-  apiKey: "AIzaSyCukYeIwM1YCkYGJXpn2dupNzLy2t39EAQ",
-  authDomain: "weperform-8063b.firebaseapp.com",
-  databaseURL: "https://weperform-8063b.firebaseio.com",
-  projectId: "weperform-8063b",
-  storageBucket: "weperform-8063b.appspot.com",
-  messagingSenderId: "279863446616"
 };
 
 firebase.initializeApp(config);
